7d5b5e5327c55650350e7eb571e4ad6f6d4d2ffd
Yoav Abrahami <yoav@wix.com>
2012-09-03 20:03:56 +0300

    refactoring to make the code a bit clearer and added more comments

--- src/main/java/com/wixpress/testapp/controller/SampleAppController.java
+++ src/main/java/com/wixpress/testapp/controller/SampleAppController.java
@@ -9,12 +9,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.AbstractController;
 
 import javax.annotation.Resource;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import java.util.UUID;
 
 /**
@@ -31,6 +27,14 @@ public class SampleAppController
 
     protected AuthenticationResolver authenticationResolver = new AuthenticationResolver(new ObjectMapper());
 
+    /**
+     * VIEW - Widget Endpoint
+     * @param model - model used by the view template widget.vm
+     * @param instance - the signed instance parameter
+     * @param target - the target parameter
+     * @param width - the width of the widget IFrame
+     * @return the template widget.vm name
+     */
     @RequestMapping(value = "/widget", method = RequestMethod.GET)
     public String widget(Model model,
                          @RequestParam String instance,
@@ -38,57 +42,53 @@ public class SampleAppController
                          @RequestParam Integer width)
     {
         WixSignedInstance wixSignedInstance = authenticationResolver.unsignInstance(sampleApp.getApplicationSecret(), instance);
-        SampleAppInstance appInstance = sampleApp.getAppInstance(wixSignedInstance);
-
-        if(appInstance == null) //new Instance created
-        {
-            appInstance = sampleApp.addAppInstance(wixSignedInstance);
-        }
-
-        appInstance.setWidth(width);
-        model.addAttribute("appInstance", appInstance);
-
-        return "widget";
+        return viewWidget(model, width, wixSignedInstance);
 
     }
 
-    // Not implemented yet
-//    @RequestMapping(value = "/section", method = RequestMethod.GET)
-    public String section(Model model,
-                          @RequestParam String instance,
-                          @RequestParam(required = false) Integer width)
-    {
-        WixSignedInstance wixSignedInstance = authenticationResolver.unsignInstance(sampleApp.getApplicationSecret(), instance);
-        SampleAppInstance appInstance = sampleApp.getAppInstance(wixSignedInstance);
-
-        if(appInstance == null) //new Instance created
-        {
-            appInstance = sampleApp.addAppInstance(wixSignedInstance);
-        }
-
-        appInstance.setWidth(width);
-        model.addAttribute("appInstance", appInstance);
-
-        return "settings";
-    }
-
+    /**
+     * VIEW - Setting Endpoint
+     * @param model - model used by the view template setting.vm
+     * @param instance - the signed instance parameter
+     * @param width - the width of the setting IFrame
+     * @return the template setting.vm name
+     */
     @RequestMapping(value = "/settings", method = RequestMethod.GET)
     public String settings(Model model,
                            @RequestParam String instance,
                            @RequestParam(required = false) Integer width)
     {
         WixSignedInstance wixSignedInstance = authenticationResolver.unsignInstance(sampleApp.getApplicationSecret(), instance);
-        SampleAppInstance appInstance = sampleApp.getAppInstance(wixSignedInstance);
+        return viewSettings(model, width, wixSignedInstance);
+    }
 
-        if(appInstance == null) //new Instance created
-        {
-            appInstance = sampleApp.addAppInstance(wixSignedInstance);
-        }
+    private String viewWidget(Model model, Integer width, WixSignedInstance wixSignedInstance) {
+        SampleAppInstance appInstance = loadOrCreateAppInstance(wixSignedInstance);
+
+        appInstance.setWidth(width);
+        model.addAttribute("appInstance", appInstance);
+
+        return "widget";
+    }
+
+    private String viewSettings(Model model, Integer width, WixSignedInstance wixSignedInstance) {
+        SampleAppInstance appInstance = loadOrCreateAppInstance(wixSignedInstance);
 
         appInstance.setWidth(width);
         model.addAttribute("appInstance", appInstance);
 
         return "settings";
+    }
+
+    private SampleAppInstance loadOrCreateAppInstance(WixSignedInstance wixSignedInstance) {
+        SampleAppInstance appInstance = sampleApp.getAppInstance(wixSignedInstance);
+
+        if(appInstance == null)
+        {
+            // new Instance created
+            appInstance = sampleApp.addAppInstance(wixSignedInstance);
+        }
+        return appInstance;
     }
 
 
@@ -111,54 +111,64 @@ public class SampleAppController
 
     ///// For Testing purposes only !! /////
 
+    /**
+     * VIEW - Widget Endpoint for testing
+     * This endpoint does not implement the Wix API. It can be used directly to test the application from any browser,
+     * substituting the signed instance parameter with explicit values given as URL parameters
+     *
+     * @param model - model used by the view template widget.vm
+     * @param instanceId - the instance id member of the signed instance
+     * @param width - the width of the widget IFrame
+     * @return the template widget.vm name
+     */
     @RequestMapping(value = "/widgetstandalone", method = RequestMethod.GET)
     public String widgetStandAlone(Model model,
-                                   String instanceId,
-                                   Integer width)
+                                   @RequestParam String instanceId,
+                                   @RequestParam Integer width)
     {
-        UUID uuid = null;
-        try {
-            uuid = UUID.fromString(instanceId);
-        } catch (Exception ignored) {}
-
-        SampleAppInstance appInstance = sampleApp.getAppInstance(uuid);
-
-        if(appInstance == null) //new Instance created
-        {
-            appInstance = sampleApp.addAppInstance(new WixSignedInstance(uuid, new DateTime(), null, ""));
-        }
-
-        appInstance.setWidth(width);
-        model.addAttribute("appInstance", appInstance);
-
-        return "widget";
+        WixSignedInstance wixSignedInstance = createTestSignedInstance(instanceId);
+        return viewWidget(model, width, wixSignedInstance);
     }
 
+    /**
+     * VIEW - Setting Endpoint for testing
+     * This endpoint does not implement the Wix API. It can be used directly to test the application from any browser,
+     * substituting the signed instance parameter with explicit values given as URL parameters
+     *
+     * @param model - model used by the view template setting.vm
+     * @param instanceId - the instance id member of the signed instance
+     * @param width - the width of the setting IFrame
+     * @return the template setting.vm name
+     */
     @RequestMapping(value = "/settingsstandalone", method = RequestMethod.GET)
     public String settingsStandAlone(Model model,
-                                     String instanceId,
-                                     Integer width)
+                                     @RequestParam String instanceId,
+                                     @RequestParam Integer width)
     {
-        UUID uuid = null;
-        try {
-            uuid = UUID.fromString(instanceId);
-        } catch (Exception ignored) {}
-
-        SampleAppInstance appInstance = sampleApp.getAppInstance(uuid);
-
-        if(appInstance == null) //new Instance created
-        {
-            appInstance = sampleApp.addAppInstance(new WixSignedInstance(uuid, new DateTime(), null, ""));
-        }
-
-        appInstance.setWidth(width);
-        model.addAttribute("appInstance", appInstance);
-
-        return "settings";
+        WixSignedInstance wixSignedInstance = createTestSignedInstance(instanceId);
+        return viewSettings(model, width, wixSignedInstance);
     }
 
-    ///// Defaults Override /////
+    private WixSignedInstance createTestSignedInstance(String instanceId) {
+        try {
+            UUID instanceUuid = UUID.fromString(instanceId);
+            return new WixSignedInstance(instanceUuid, new DateTime(), null, "");
+        } catch (Exception ignored) {
+            throw new RuntimeException(String.format("failed parsing instanceId [%s]. Valid values are GUIDs of the form aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa", instanceId));
+        }
+    }
 
+    /**
+     * AJAX - operation which allows to change the applicationId and applicationSecret of this app during runtime.
+     * It can be used during app development to update the running app applicationId and applicationSecret after
+     * you register your application with Wix.
+     *
+     * DELETE THIS OPERATION BEFORE SUBMITTING YOUR APPLICATION WITH WIX
+     *
+     * @param applicationID - the application id
+     * @param applicationSecret - the application secret
+     * @return void result indicating success
+     */
     @RequestMapping(value = "/sampleappupdate", method = RequestMethod.GET)
     @ResponseBody
     public Result<Void> sampleAppUpdate(@RequestParam String applicationID,
